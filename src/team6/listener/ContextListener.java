5c77b35bc24f884d3783417909342ffa7bccc399
Tai Nguyen <ng.xtai93@gmail.com>
2017-11-09 23:51:16 -0600

    add ServletLogin, ContextListener

---
+++ src/team6/listener/ContextListener.java
@@ -0,0 +1,13 @@
+package team6.listener;
+
+import javax.servlet.*;
+
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextDestroyed(ServletContextEvent e) {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent e) {
+    }
+}
--- src/team6/servlet/ServletLogin.java
+++ src/team6/servlet/ServletLogin.java
@@ -8,17 +8,16 @@ import javax.servlet.http.*;
 @WebServlet("/login")
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
 		doGet(request, response);
 	}
 
