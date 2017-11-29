f6d3968081aae2e35ec412c5ca713525022bea53
Tai Nguyen <ng.xtai93@gmail.com>
2017-11-29 17:49:05 -0600

    setup mongodb db

--- src/team6/listener/ContextListener.java
+++ src/team6/listener/ContextListener.java
@@ -2,20 +2,26 @@ package team6.listener;
 
 import javax.servlet.*;
 
+import team6.dao.MongoDBDatabaseOperator;
 import team6.dao.MySQLDatabaseOperator;
 
 public class ContextListener implements ServletContextListener {
 	
 	private MySQLDatabaseOperator mySql = MySQLDatabaseOperator.INSTANCE;
-    @Override
+	private MongoDBDatabaseOperator mongoDb = MongoDBDatabaseOperator.INSTANCE;
+	
+	@Override
     public void contextDestroyed(ServletContextEvent e) {
     	closeMySqlConnection();
+    	closeMongoDbConnection();
     }
 
 	@Override
-    public void contextInitialized(ServletContextEvent e) {
-    	initMySqlConnection(e.getServletContext());
-    }
+	public void contextInitialized(ServletContextEvent e) {
+		ServletContext sc = e.getServletContext();
+		initMySqlConnection(sc);
+		initMongoDbConnection(sc);
+	}
 
 	private void initMySqlConnection(ServletContext sc) {
 		mySql.initConnection();
@@ -24,5 +30,13 @@ public class ContextListener implements ServletContextListener {
 	
     private void closeMySqlConnection() {
     	mySql.closeConnection();
+	}
+
+	private void initMongoDbConnection(ServletContext sc) {
+		mongoDb.initConnection();
+	}
+
+	private void closeMongoDbConnection() {
+		mongoDb.closeConnection();
 	}
 }
