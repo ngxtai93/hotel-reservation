59e15c54f390d2a6689d4706aeb31c3689678b48
Tai Nguyen <ng.xtai93@gmail.com>
2017-11-30 15:08:25 -0600

    add more data

--- WebContent/resources/sql/setup.sql
+++ WebContent/resources/sql/setup.sql
@@ -67,7 +67,7 @@ CREATE TABLE `hotel` (
   `image_link` text,
   `del_flag` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`seq_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +76,7 @@ CREATE TABLE `hotel` (
 
 LOCK TABLES `hotel` WRITE;
 /*!40000 ALTER TABLE `hotel` DISABLE KEYS */;
-INSERT INTO `hotel` VALUES (1,1,'Hampton Inn Majestic Chicago Theatre District','22 W. Monroe','The Hampton Majestic experience is unlike any other, attached to CIBC Theatre and steps from State Street, museums, Millennium Park and Willis Tower.','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,',0),(2,2,'theWit Chicago - a DoubleTree by Hilton Hotel','201 N. State Street','Award-winning downtown hotel and rooftop bar offers a dramatic sensory experience, blending the amenities of a resort with the flair of a boutique hotel','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,11.jpg,12.jpg,',0),(3,1,'Palmer House - A Hilton Hotel','17 East Monroe Street','AAA Four Diamond, historic hotel located in the heart of the Loop. Steps from theatre, shopping, the Art Institute of Chicago and Millennium Park.','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,11.jpg,12.jpg,',0);
+INSERT INTO `hotel` VALUES (1,1,'Hampton Inn Majestic Chicago Theatre District','22 W. Monroe','The Hampton Majestic experience is unlike any other, attached to CIBC Theatre and steps from State Street, museums, Millennium Park and Willis Tower.','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,',0),(2,2,'theWit Chicago - a DoubleTree by Hilton Hotel','201 N. State Street','Award-winning downtown hotel and rooftop bar offers a dramatic sensory experience, blending the amenities of a resort with the flair of a boutique hotel','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,11.jpg,12.jpg,',0),(3,1,'Palmer House - A Hilton Hotel','17 East Monroe Street','AAA Four Diamond, historic hotel located in the heart of the Loop. Steps from theatre, shopping, the Art Institute of Chicago and Millennium Park.','1.jpg,2.jpg,3.jpg,4.jpg,5.jpg,6.jpg,7.jpg,8.jpg,9.jpg,10.jpg,11.jpg,12.jpg,',0),(4,4,'DoubleTree Suites by Hilton Hotel New York City - Times Square','1568 Broadway','All-suite hotel surrounded by Broadway theaters and only steps from Radio City Music Hall, Rockefeller Center, TKTS ticket booth and Central Park.','RA7AD2SIBKJHYHMFUP.jpg,ERFJ3NG27JXKMR4FTO.jpg,32D0XHGR8XIH2R838Q.jpg,Q83L2K7R1ONTEHKNC5.jpg,RWL7QC5CMFKU7WEDWQ.jpg,KQD2TRYLVORK4N618S.jpg,BR5AEBZSGRMKIE4GWM.jpg,V2CAR2M0HNMVAKSP00.jpg,HJ6TC5DUVQYQC7IL41.jpg,I7PH0J328DD2N1VSS7.jpg,30EWVMRGR180SKZ08G.jpg,EOYIIO9C9MOILCUL45.jpg,',0),(5,5,'Conrad Miami','1395 Brickell Avenue','A contemporary luxury hotel overlooking the stunning city and Biscayne Bay, boasting a beautiful spa, rooftop pool, and signature restaurant and lounge.','ISSQX2VNEKGXAJQIZ4.jpg,JXQHW938GXY6HHLKMJ.jpg,4AN61WP8Y42SZCI5KD.jpg,13SPAN2JUV5PHM67Y9.jpg,I6MO5AN4NRH1U95A26.jpg,CQ1PUVKCDTUWMDLB17.jpg,R1L2SDNX1F2RSVIN6K.jpg,NXAWAQTP7CVMKC7OYU.jpg,H8K8K77BFFVMU80A1G.jpg,BFT1IWTXZTGZF2ZQJM.jpg,CHZB4XDHGVD0ACAH08.jpg,',0);
 /*!40000 ALTER TABLE `hotel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +93,7 @@ CREATE TABLE `location` (
   `state` varchar(10) DEFAULT NULL,
   `zip` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`seq_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +102,7 @@ CREATE TABLE `location` (
 
 LOCK TABLES `location` WRITE;
 /*!40000 ALTER TABLE `location` DISABLE KEYS */;
-INSERT INTO `location` VALUES (1,'Chicago','IL','60603'),(2,'Chicago','IL','60601');
+INSERT INTO `location` VALUES (1,'Chicago','IL','60603'),(2,'Chicago','IL','60601'),(4,'New York','NY','10036'),(5,'Miami','FL','33131');
 /*!40000 ALTER TABLE `location` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +177,7 @@ DROP TABLE IF EXISTS `room_type`;
 CREATE TABLE `room_type` (
   `seq_no` int(11) NOT NULL AUTO_INCREMENT,
   `hotel` int(11) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `bed_type` varchar(255) DEFAULT NULL,
   `bed_amount` int(11) DEFAULT NULL,
   `people_no` int(11) DEFAULT NULL,
@@ -190,7 +190,7 @@ CREATE TABLE `room_type` (
   `image` text,
   `del_flag` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`seq_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,7 +199,7 @@ CREATE TABLE `room_type` (
 
 LOCK TABLES `room_type` WRITE;
 /*!40000 ALTER TABLE `room_type` DISABLE KEYS */;
-INSERT INTO `room_type` VALUES (1,1,'1 King Bed','king_bed',1,2,'City',1,1,139,3,'523,312','1.jpg',0),(2,1,'2 Queen Bed','queen_bed',2,4,'City',1,1,199,4,'101,102','1.jpg',0),(3,2,'CLASSIC KING ROOM NONSMOKING','king_bed',1,2,'City',1,1,499,9,'201,203,204,','SVZUMMYDD72WIE0BG5.jpg',0),(4,2,'CLASSIC TWO QUEEN ROOM NONSMOKING','queen_bed',2,4,'City',1,1,429,9,'300,','QHRVADLZXHEV3JS4FF.jpg',0);
+INSERT INTO `room_type` VALUES (1,1,'1 King Bed','king_bed',1,2,'City',1,1,139,3,'523,312','1.jpg',0),(2,1,'2 Queen Bed','queen_bed',2,4,'City',1,1,199,4,'101,102','1.jpg',0),(3,2,'CLASSIC KING ROOM NONSMOKING','king_bed',1,2,'City',1,1,499,9,'201,203,204,','SVZUMMYDD72WIE0BG5.jpg',0),(4,2,'CLASSIC TWO QUEEN ROOM NONSMOKING','queen_bed',2,4,'City',1,1,429,9,'300,','QHRVADLZXHEV3JS4FF.jpg',0),(5,4,'1 KG BED STD SUITE-LIV RM SOFA BED-NON SMK','king_bed',1,2,'City',1,1,549,11,'100,101,102,103,104,','GNA03CZTRZHVR5JJZ0.jpg',0),(6,4,'1 QN BED PREM SUITE-LIV RM SOFA BED-CITY VW','queen_bed',1,2,'City',1,1,549,11,'201,203,204,','U8I1GAYFC7A527OBRE.jpg',0),(7,5,'CITY VIEW ROOM WITH 1 KING BED','king_bed',1,1,'City',1,1,299,0,'100,101,102,103,104','GNA03CZTRZHVR5JJZ0.jpg',0),(8,5,'BAY VIEW ROOM WITH KING SIZE BED','king_bed',2,4,'Bay',1,1,319,7,'201,203,204,','1FWBIH3PXHRIBMKBLK.jpg',0);
 /*!40000 ALTER TABLE `room_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-29 23:09:00
+-- Dump completed on 2017-11-30 15:07:21
